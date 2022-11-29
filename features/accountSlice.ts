8aa1c505002fd5e8e270bbeb1bf43946a00362ce
oldhand7 <oldhanddev@gmail.com>
2022-11-29 04:59:43 -0500

    add space

--- features/accountSlice.ts
+++ features/accountSlice.ts
@@ -34,6 +34,7 @@ export const connectAccount: any = createAsyncThunk(
       getState().config;
 
     let ethereum:any;
+    
 
     const providerForWalletType: any = async (walletType: any) => {
       switch (walletType) {
